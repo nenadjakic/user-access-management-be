28d5a68056db836f222624d4a241a97cde1ce531
nenadjakic <89663559+nenadjakic@users.noreply.github.com>
2025-07-29 19:01:00 +0200

    Removed test migration file.

--- src/main/resources/db/migration/data/V002__insert_clients.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO "security".tenants (id, name, private_key_path, public_key_path, is_active)
-VALUES('46d1aa31-054f-43dc-b502-03012fe13e87', 'local_client', 'src/main/resources/private_key.pem', 'src/main/resources/public_key.pem', true);
